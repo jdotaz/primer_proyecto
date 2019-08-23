c429b9e332949690f2d7cc1153c0805cc6842865
jdotaz <50921075+jdotaz@users.noreply.github.com>
2019-08-23 09:45:18 -0500

    Update main.cpp

--- proyecto/main.cpp
+++ proyecto/main.cpp
@@ -4,6 +4,24 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+  int a;
+  int b;
+  int c;
+    cout << "ingrese el primer numero:";
+    cin >> a;
+    cout << "ingrese el segundo numero:";
+    cin >> b;
+    cout << "ingrese el tercer numero:";
+    cin >> c;
+  if ((a>b)&&(a>c)){
+     cout << "el numero mayor es:"<< a << endl;
+    }
+  if ((b>a)&&(b>c)){
+     cout << "el numero mayor es:"<<  b << endl;
+    }
+    
+  if ((c>b)&&(c>a)){
+     cout << "el numero mayor es:"<< c << endl;
+    }
+  return 0;
 }
